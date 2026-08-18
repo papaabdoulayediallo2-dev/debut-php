8497053a1719c50a90bf216a3e767eb7041d4333
papaabdoulayediallo2-dev <papaabdoulayediallo2@gmail.com>
2026-08-18 00:06:11 +0000

    renommage du fichier

--- function.php
+++ function.php
@@ -8,4 +8,14 @@ function checkbox(string $name, string $value , array $data): string{
     <input type="checkbox" name="{$name}[]" value="$value" $attributes>
     HTML;
 }
+
+function radio(string $name, string $value , array $data): string{
+    $attributes = '';
+    if(isset($data[$name]) && in_array($value, $data[$name])){
+        $attributes .= 'checked';
+    }
+    return <<<HTML
+    <input type="radio" name="{$name}" value="$value" $attributes>
+    HTML;
+}
 ?>
